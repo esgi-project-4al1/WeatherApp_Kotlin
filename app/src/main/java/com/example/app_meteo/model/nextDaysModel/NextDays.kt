4a40c500b9715a29130acd2ee54f25ff827a3087
Medmat0 <yassineelmatrour@gmail.com>
2024-01-18 15:24:39 +0100

    Adding NextDays model

--- app/src/main/java/com/example/app_meteo/model/nextDaysModel/NextDays.kt
+++ app/src/main/java/com/example/app_meteo/model/nextDaysModel/NextDays.kt
@@ -1,4 +1,9 @@
 package com.example.app_meteo.model.nextDaysModel
 
-class NextDays {
-}
+data class NextDays(
+    val sunrise: List<String>,
+    val sunset: List<String>,
+    val temperature_max: List<Double>,
+    val temperature_min: List<Double>,
+    val time: List<String>,
+    val weathercode: List<Int>)
--- app/src/main/java/com/example/app_meteo/model/nextDaysModel/NextDaysModel.kt
+++ app/src/main/java/com/example/app_meteo/model/nextDaysModel/NextDaysModel.kt
@@ -1,4 +1,13 @@
 package com.example.app_meteo.model.nextDaysModel
 
-class NextDaysModel {
-}
+
+data class NextDaysModel(
+    val daily : NextDays,
+    val elevation: Double,
+    val generationtime_ms: Double,
+    val latitude: Double,
+    val longitude: Double,
+    val timezone: String,
+    val timezone_abbreviation: String,
+    val utc_offset_seconds: Int
+)
