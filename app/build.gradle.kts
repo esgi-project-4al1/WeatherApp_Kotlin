2e66bf8d26ec191b14a9a8b6473e7d1ef76ec14e
Medmat0 <yassineelmatrour@gmail.com>
2024-01-18 15:27:30 +0100

    Adding service for nextdays & retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/app_meteo/data/remote/NextdaysService.kt
+++ app/src/main/java/com/example/app_meteo/data/remote/NextdaysService.kt
@@ -1,5 +1,18 @@
 package com.example.app_meteo.data.remote
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import com.example.app_meteo.model.nextDaysModel.NextDaysModel
+
 interface NextdaysService {
+    @GET("/v1/forecast")
+    suspend fun getWeather(
+        @Query("latitude") latitude: String,
+        @Query("longitude") longitude: String,
+        @Query("daily") dailyParameters: List<String>,
+        @Query("timezone") timezone: String,
+        @Query("forecast_days") forecastDays: Int
+    ): Response<NextDaysModel>
 
 }
