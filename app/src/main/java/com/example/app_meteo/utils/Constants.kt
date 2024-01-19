686a02f201dd6b9c31f7558787e7714e102b1a43
Medmat0 <yassineelmatrour@gmail.com>
2024-01-19 16:55:55 +0100

    Adding constanst val the MVVM

---
+++ app/src/main/java/com/example/app_meteo/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.app_meteo.utils
+
+object Constants {
+
+    const val NEXT7DAYS_WEATHER_DAYS_LIMIT = 8
+    const val OpenWeatherMap_API_BASE_URL = "https://api.openweathermap.org/"
+    const val api_key =  "219977471c4344fab98220353231112"
+
+
+
+}
